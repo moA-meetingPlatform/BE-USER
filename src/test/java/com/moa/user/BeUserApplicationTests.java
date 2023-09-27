6a626015bb6804e2f21ca580cad05d491d1cc6a1
bnuri00 <bnurik104@gmail.com>
2023-09-27 23:10:11 +0900

    feat: 프로젝트 초기 설정 - mysql db 이중화 설정

---
+++ src/test/java/com/moa/user/BeUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moa.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
