cc3973cce7f5a7cd6a8d1481808fac6d9cbda0bc
bnuri00 <bnurik104@gmail.com>
2023-10-26 17:44:30 +0900

    feat: 스웨거 설정 추가 - swagger Security 인증 컴포넌트 설정 - info 설정

---
+++ src/main/java/com/moa/global/config/SwaggerConfig.java
@@ -0,0 +1,59 @@
+package com.moa.global.config;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public GroupedOpenApi publicApi() {
+
+		String[] paths = { "/api/v1/**" };
+
+		return GroupedOpenApi.builder()
+			.group("user-api")
+			.pathsToMatch(paths)
+			.build();
+	}
+
+
+	@Bean
+	public OpenAPI openAPI() {
+		// Security 스키마 설정
+		SecurityScheme bearerAuth = new SecurityScheme()
+			.type(SecurityScheme.Type.HTTP)
+			.scheme("Bearer")
+			.bearerFormat("Authorization")
+			.in(SecurityScheme.In.HEADER)
+			.name(HttpHeaders.AUTHORIZATION);
+
+		// Security 요청 설정
+		SecurityRequirement addSecurityItem = new SecurityRequirement();
+		addSecurityItem.addList("Authorization");
+
+		Info info = new Info()
+			.version("v1.0.0")
+			.title("MOA APP API")
+			.description("moa user service");
+
+		return new OpenAPI()
+			.info(info)
+			.components(new Components().addSecuritySchemes("Authorization", bearerAuth))
+			.security(List.of(addSecurityItem));
+	}
+
+}
