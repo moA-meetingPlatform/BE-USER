94892eec5f5bf26b11d1deb6b256ee62339a89a6
bnuri00 <bnurik104@gmail.com>
2023-11-03 21:06:35 +0900

    feat: 시큐리티에서 user detail 유저 유효성 검증 추가, 유저 엔티티 rename
    user detail isAccountNonExpired, isEnabled 메소드 수정
    
    isAccountNonExpired
    - workplaceVerified가 false이면 계정 만료
    
    isEnabled 메소드
    - userSoftDelete가 true이고, 탈퇴 날짜(updateDatetime)가 7일 이전이면 비활성화
    
    #36

--- src/main/java/com/moa/user/domain/User.java
+++ src/main/java/com/moa/user/domain/User.java
@@ -2,7 +2,6 @@ package com.moa.user.domain;
 
 
 import com.moa.global.domain.BaseEntity;
-import com.moa.workplace.domain.Workplace;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
@@ -10,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
@@ -55,26 +55,26 @@ public class User extends BaseEntity implements UserDetails {
 	@Column(nullable = false, length = 100, name = "nickname")
 	private String nickname;
 
-	@Column(columnDefinition = "tinyint default 1", name = "account_use")
-	private Boolean accountUse;
+	@Column(columnDefinition = "tinyint default 0", name = "user_soft_delete")
+	private Boolean userSoftDelete;
 
-	@Column(length = 100, name = "introduction")
-	private String introduction;
+	@Column(length = 100, name = "user_introduce")
+	private String userIntroduce;
 
 	@Column(length = 100, name = "profile_image_url")
 	private String profileImage;
 
-	@Column(name = "email_notifications")
-	private Boolean emailNotifications;
+	@Column(name = "email_notification_status")
+	private Boolean emailNotificationStatus;
 
-	@Column(name = "sms_notifications")
-	private Boolean smsNotifications;
+	@Column(name = "sms_notification_status")
+	private Boolean smsNotificationStatus;
 
-	@Column(name = "push_notifications")
-	private Boolean pushNotifications;
+	@Column(name = "push_notification_status")
+	private Boolean pushNotificationStatus;
 
-	@Column(name = "device_token")
-	private String deviceToken;
+	@Column(name = "user_device_token")
+	private String userDeviceToken;
 
 	@Column(name = "workplace_verification_date")
 	private LocalDate workplaceVerificationDate;
@@ -82,10 +82,8 @@ public class User extends BaseEntity implements UserDetails {
 	@Column(columnDefinition = "tinyint default 1", name = "workplace_verified")
 	private Boolean workplaceVerified;
 
-	// 외래키
-	@ManyToOne
-	@JoinColumn(name = "workplace_id")
-	private Workplace workplaceId;
+	@Column(name = "company_id", nullable = false)
+	private Integer companyId;  // Company 테이블의 id 저장됨
 
 
 	@Override
@@ -100,9 +98,14 @@ public class User extends BaseEntity implements UserDetails {
 	}
 
 
+	/**
+	 * workplaceVerified가 false이면 계정 만료
+	 *
+	 * @return 계정 만료 여부 (true: 만료 안됨, false: 만료됨)
+	 */
 	@Override
 	public boolean isAccountNonExpired() {
-		return true;
+		return this.workplaceVerified;
 	}
 
 
@@ -118,9 +121,16 @@ public class User extends BaseEntity implements UserDetails {
 	}
 
 
+	/**
+	 * 탈퇴처리가 완료된 계정은 비활성화
+	 *
+	 * @return 계정 활성화 여부 (true: 활성화, false: 비활성화)
+	 */
 	@Override
 	public boolean isEnabled() {
-		return this.accountUse;
+		// 탈퇴처리가 완료되었는지 확인
+		// userSoftDelete가 true이고, 탈퇴 날짜(updateDatetime)가 7일 이전이면 비활성화
+		return !(this.userSoftDelete && this.getUpdateDatetime().isBefore(LocalDateTime.now().minusDays(7)));
 	}
 
 
